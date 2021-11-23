1cc9f35bf472b7587318123108558dcc9750a95e
Tomas Hozza <thozza@redhat.com>
2021-11-23 18:59:35 +0100

    Add test proving rhsm code bug
    Add test case proving that getting RHSM secrets the "new" way does not
    work with the RHEL repo definitions as shipped by osbuild-composer. As
    a result, the code always falls back to the "old" way and uses the first
    cert found on the filesystem.
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- internal/rhsm/secrets_test.go
+++ internal/rhsm/secrets_test.go
@@ -32,6 +32,32 @@ sslclientkey = /etc/pki/entitlement/789-key.pem
 sslclientcert = /etc/pki/entitlement/101112.pem
 metadata_expire = 86400
 enabled_metadata = 0
+
+[rhel-8-for-x86_64-appstream-rpms]
+name = Red Hat Enterprise Linux 8 for x86_64 - AppStream (RPMs)
+baseurl = https://cdn.redhat.com/content/dist/rhel8/$releasever/x86_64/appstream/os
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+sslverify = 1
+sslcacert = /etc/rhsm/ca/redhat-uep.pem
+sslclientkey = /etc/pki/entitlement/121-key.pem
+sslclientcert = /etc/pki/entitlement/121.pem
+metadata_expire = 86400
+enabled_metadata = 1
+
+[rhel-8-for-x86_64-baseos-rpms]
+name = Red Hat Enterprise Linux 8 for x86_64 - BaseOS (RPMs)
+baseurl = https://cdn.redhat.com/content/dist/rhel8/$releasever/x86_64/baseos/os
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+sslverify = 1
+sslcacert = /etc/rhsm/ca/redhat-uep.pem
+sslclientkey = /etc/pki/entitlement/121-key.pem
+sslclientcert = /etc/pki/entitlement/121.pem
+metadata_expire = 86400
+enabled_metadata = 1
 `
 
 func TestParseRepoFile(t *testing.T) {
@@ -46,4 +72,16 @@ func TestParseRepoFile(t *testing.T) {
 	assert.Equal(t, secrets.SSLCACert, "/etc/rhsm/ca/redhat-uep.pem", "Unexpected path to the CA certificate")
 	assert.Equal(t, secrets.SSLClientCert, "/etc/pki/entitlement/456.pem", "Unexpected path to the client cert")
 	assert.Equal(t, secrets.SSLClientKey, "/etc/pki/entitlement/123-key.pem", "Unexpected path to the client key")
+
+	// BaseURL from `repositories/rhel-85.json`
+	x86ComposerRepoBaseURL := "https://cdn.redhat.com/content/dist/rhel8/8.5/x86_64/baseos/os"
+	x86ArchName := "x86_64"
+	// value returned by rhel_85 Distro.Releasever()
+	rhel85distroReleasever := "8"
+
+	secrets, err = subscriptions.GetSecretsForBaseurl(x86ComposerRepoBaseURL, x86ArchName, rhel85distroReleasever)
+	require.NoError(t, err, "Failed to get secrets for a baseurl")
+	assert.Equal(t, secrets.SSLCACert, "/etc/rhsm/ca/redhat-uep.pem", "Unexpected path to the CA certificate")
+	assert.Equal(t, secrets.SSLClientCert, "/etc/pki/entitlement/121.pem", "Unexpected path to the client cert")
+	assert.Equal(t, secrets.SSLClientKey, "/etc/pki/entitlement/121-key.pem", "Unexpected path to the client key")
 }
