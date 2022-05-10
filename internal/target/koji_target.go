edf1df9d2cf08d984de1dba370b1deba26af0356
Tomas Hozza <thozza@redhat.com>
2022-05-10 14:57:42 +0200

    target: add `KojiTargetResultOptions`
    Add `KojiTargetResultOptions` which contains the values contained in
    `OSBuildKojiJobResult`, but not in `OSBuildJobResult`.

--- internal/target/koji_target.go
+++ internal/target/koji_target.go
@@ -18,3 +18,16 @@ func (KojiTargetOptions) isTargetOptions() {}
 func NewKojiTarget(options *KojiTargetOptions) *Target {
 	return newTarget("org.osbuild.koji", options)
 }
+
+type KojiTargetResultOptions struct {
+	HostOS    string `json:"host_os"`
+	Arch      string `json:"arch"`
+	ImageHash string `json:"image_hash"`
+	ImageSize uint64 `json:"image_size"`
+}
+
+func (KojiTargetResultOptions) isTargetResultOptions() {}
+
+func NewKojiTargetResult(options *KojiTargetResultOptions) *TargetResult {
+	return newTargetResult("org.osbuild.koji", options)
+}
