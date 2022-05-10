a8e170118c478eb0c4c751b1cf053e70d09968fd
Tomas Hozza <thozza@redhat.com>
2022-05-10 14:35:07 +0200

    worker: standardize logging in OCI target
    The OCI target used `log`, instead of `logWithId` for logging messages.
    Modify the code to be consistent with other targets.

--- cmd/osbuild-worker/jobimpl-osbuild.go
+++ cmd/osbuild-worker/jobimpl-osbuild.go
@@ -5,7 +5,6 @@ import (
 	"crypto/rand"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math"
 	"math/big"
 	"os"
@@ -662,8 +661,8 @@ func (impl *OSBuildJobImpl) Run(job worker.Job) error {
 				osbuildJobResult.JobError = clienterrors.WorkerClientError(clienterrors.ErrorInvalidConfig, err.Error())
 				return nil
 			}
-			log.Print("[OCI] 🔑 Logged in OCI")
-			log.Print("[OCI] ⬆ Uploading the image")
+			logWithId.Info("[OCI] 🔑 Logged in OCI")
+			logWithId.Info("[OCI] ⬆ Uploading the image")
 			file, err := os.Open(path.Join(outputDirectory, exportPath, options.FileName))
 			if err != nil {
 				osbuildJobResult.JobError = clienterrors.WorkerClientError(clienterrors.ErrorInvalidConfig, err.Error())
@@ -683,7 +682,7 @@ func (impl *OSBuildJobImpl) Run(job worker.Job) error {
 				osbuildJobResult.JobError = clienterrors.WorkerClientError(clienterrors.ErrorInvalidConfig, err.Error())
 				return nil
 			}
-			log.Print("[OCI] 🎉 Image uploaded and registered!")
+			logWithId.Info("[OCI] 🎉 Image uploaded and registered!")
 
 			osbuildJobResult.TargetResults = append(
 				osbuildJobResult.TargetResults,
